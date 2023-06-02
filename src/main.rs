df418ce971eddee81a143389b119afb0ba951a8f
rhian-cs <rhian.luis.cs+github@gmail.com>
2023-06-02 19:27:45 -0300

    Add single threaded directory hashing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs, path::Path};
+
 use clap::Parser;
 use hash_calculator::file_hash_calculator;
 
@@ -9,7 +11,34 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let hex = file_hash_calculator::calculate(&args.directory).unwrap();
+    let path = args.directory;
 
-    println!("{hex}");
+    if !Path::new(&path).try_exists().unwrap() {
+        panic!("Directory or file {path} does not exist!");
+    }
+
+    calculate_hash_for_path(&path);
+}
+
+fn calculate_hash_for_path(path: &str) {
+    if Path::new(path).is_dir() {
+        calculate_hash_for_directory(&path);
+        return;
+    }
+
+    calculate_hash_for_file(&path);
+}
+
+fn calculate_hash_for_directory(path: &str) {
+    let paths = fs::read_dir(&path).unwrap();
+
+    for path in paths {
+        calculate_hash_for_path(&path.unwrap().path().to_str().unwrap());
+    }
+}
+
+fn calculate_hash_for_file(path: &str) {
+    let hex = file_hash_calculator::calculate(&path).unwrap();
+
+    println!("{path}\t{hex}");
 }
