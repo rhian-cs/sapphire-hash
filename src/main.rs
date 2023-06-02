e86f815b9f23d7f0e2abb0f17d026d04d0fb9290
rhian-cs <rhian.luis.cs+github@gmail.com>
2023-06-02 18:57:39 -0300

    Add clap for CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use clap::Parser;
 use hash_calculator::file_hash_calculator;
 
+#[derive(Parser)]
+struct Args {
+    #[arg()]
+    directory: String,
+}
+
 fn main() {
-    let hex = file_hash_calculator::calculate("tmp/hello-world.txt").unwrap();
+    let args = Args::parse();
+    let hex = file_hash_calculator::calculate(&args.directory).unwrap();
 
     println!("{hex}");
 }
