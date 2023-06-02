e80e236db165d556b1e099b56885b4c14a8c782c
rhian-cs <rhian.luis.cs+github@gmail.com>
2023-06-01 21:29:11 -0300

    Initial hash calculator version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+};
+
+use crypto::{digest::Digest, sha1::Sha1};
+
 fn main() {
-    println!("Hello, world!");
+    let mut hasher = Sha1::new();
+
+    let filename = "tmp/hello-world.txt";
+
+    let file = File::open(filename).unwrap();
+
+    let mut reader = BufReader::new(file);
+
+    const BUFSIZE: usize = 8192;
+    let mut buf: [u8; BUFSIZE] = [0; BUFSIZE];
+
+    loop {
+        let bytes_read = reader.read(&mut buf).unwrap();
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        let bytes = &buf[0..bytes_read];
+
+        hasher.input(bytes);
+    }
+
+    let hex = hasher.result_str();
+
+    println!("{hex}");
 }
