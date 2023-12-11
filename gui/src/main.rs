e1dbb8a3972a3382f6affff29680fedca7e6695d
rhian-cs <rhian.luis.cs+github@gmail.com>
2023-12-11 11:41:59 -0300

    Show result after calculation is complete

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -13,6 +13,7 @@ async fn main() -> Result<(), slint::PlatformError> {
         let window = window.as_weak().unwrap();
         move || window.set_input_path(open_directory_input().into())
     });
+
     window.on_output_path_button_clicked({
         let window = window.as_weak().unwrap();
         move || window.set_output_path(open_directory_input().into())
@@ -26,13 +27,8 @@ async fn main() -> Result<(), slint::PlatformError> {
             let input_path = window.get_input_path().to_string();
             let output_path = window.get_output_path().to_string();
 
-            let output_path = Path::new(&output_path)
-                .join(report_filename())
-                .to_str()
-                .unwrap()
-                .to_owned();
-
             window.set_error_message("".into());
+            window.set_info_message("".into());
 
             if input_path.is_empty() {
                 window.set_error_message("Please select an input directory.".into());
@@ -45,6 +41,12 @@ async fn main() -> Result<(), slint::PlatformError> {
             }
 
             window.set_buttons_enabled(false);
+
+            let output_path = Path::new(&output_path)
+                .join(report_filename())
+                .to_str()
+                .unwrap()
+                .to_owned();
 
             tokio::spawn(calculate_hashes(weak_window.clone(), input_path, output_path));
         }
@@ -63,13 +65,21 @@ fn open_directory_input() -> String {
 }
 
 async fn calculate_hashes(window: slint::Weak<MainWindow>, input_path: String, output_path: String) {
-    let result = hasher::process(input_path, HashStrategy::Sha256, ReportType::Csv(output_path)).await;
-
-    println!("Elapsed: {:?}", result.elapsed_time);
+    let result = hasher::process(input_path, HashStrategy::Sha256, ReportType::Csv(output_path.clone())).await;
 
     window
         .upgrade_in_event_loop(move |window| {
+            let count = result.processed_files_count;
+            let elapsed_time = result.elapsed_time.as_secs_f32();
+            let files_have = if count == 1 { "file has" } else { "files have" };
+
             window.set_buttons_enabled(true);
+            window.set_info_message(
+                format!(
+                    "{count} {files_have} been processed!\n\nTook {elapsed_time} seconds.\n\nThe report has been saved at:\n{output_path}",
+                )
+                .into(),
+            );
         })
         .unwrap();
 }
