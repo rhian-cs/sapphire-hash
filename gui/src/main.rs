94d5b3720c6035844295fb46041b8336ef333d9f
rhian-cs <rhian.luis.cs+github@gmail.com>
2023-12-11 10:29:45 -0300

    Add async calculation

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,7 +1,12 @@
+use std::path::Path;
+
+use recursive_hash_calculator_core::{hash_strategy::HashStrategy, hasher, report_type::ReportType};
 use rfd::FileDialog;
 
 slint::include_modules!();
-fn main() -> Result<(), slint::PlatformError> {
+
+#[tokio::main]
+async fn main() -> Result<(), slint::PlatformError> {
     let window = MainWindow::new()?;
 
     window.on_input_path_button_clicked({
@@ -14,11 +19,19 @@ fn main() -> Result<(), slint::PlatformError> {
     });
 
     window.on_calculate_button_clicked({
-        let window = window.as_weak().unwrap();
+        let weak_window = window.as_weak();
 
         move || {
-            let input_path = window.get_input_path();
-            let output_path = window.get_output_path();
+            let window = weak_window.unwrap();
+            let input_path = window.get_input_path().to_string();
+            let output_path = window.get_output_path().to_string();
+
+            let output_path = Path::new(&output_path)
+                .join(report_filename())
+                .to_str()
+                .unwrap()
+                .to_owned();
+
             window.set_error_message("".into());
 
             if input_path.is_empty() {
@@ -30,6 +43,10 @@ fn main() -> Result<(), slint::PlatformError> {
                 window.set_error_message("Please select a directory for the report to be saved.".into());
                 return;
             }
+
+            window.set_buttons_enabled(false);
+
+            tokio::spawn(calculate_hashes(weak_window.clone(), input_path, output_path));
         }
     });
 
@@ -43,4 +60,20 @@ fn open_directory_input() -> String {
         Some(path) => path.to_str().unwrap_or_default().to_owned(),
         None => String::default(),
     }
+}
+
+async fn calculate_hashes(window: slint::Weak<MainWindow>, input_path: String, output_path: String) {
+    let result = hasher::process(input_path, HashStrategy::Sha256, ReportType::Csv(output_path)).await;
+
+    println!("Elapsed: {:?}", result.elapsed_time);
+
+    window
+        .upgrade_in_event_loop(move |window| {
+            window.set_buttons_enabled(true);
+        })
+        .unwrap();
+}
+
+fn report_filename() -> String {
+    chrono::Local::now().format("%Y-%m-%d_%H%M%S.csv").to_string()
 }
