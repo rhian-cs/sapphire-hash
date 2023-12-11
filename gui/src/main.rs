cc2d7b619ad92690019aa665937b25e5f9dea227
rhian-cs <rhian.luis.cs+github@gmail.com>
2023-12-11 00:17:19 -0300

    Add file inputs

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,7 +1,46 @@
-slint::include_modules!();
+use rfd::FileDialog;
 
+slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let window = MainWindow::new()?;
 
+    window.on_input_path_button_clicked({
+        let window = window.as_weak().unwrap();
+        move || window.set_input_path(open_directory_input().into())
+    });
+    window.on_output_path_button_clicked({
+        let window = window.as_weak().unwrap();
+        move || window.set_output_path(open_directory_input().into())
+    });
+
+    window.on_calculate_button_clicked({
+        let window = window.as_weak().unwrap();
+
+        move || {
+            let input_path = window.get_input_path();
+            let output_path = window.get_output_path();
+            window.set_error_message("".into());
+
+            if input_path.is_empty() {
+                window.set_error_message("Please select an input directory.".into());
+                return;
+            }
+
+            if output_path.is_empty() {
+                window.set_error_message("Please select a directory for the report to be saved.".into());
+                return;
+            }
+        }
+    });
+
     window.run()
+}
+
+fn open_directory_input() -> String {
+    let directory = FileDialog::new().pick_folder();
+
+    match directory {
+        Some(path) => path.to_str().unwrap_or_default().to_owned(),
+        None => String::default(),
+    }
 }
