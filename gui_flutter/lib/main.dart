8482f9e7dab994e03e0ccabfaf16a2210230e31d
rhian-cs <rhian.luis.cs+github@gmail.com>
2024-03-10 21:55:05 -0300

    Integrate with flutter_rust_bridge
    The following command was run:
    flutter_rust_bridge_codegen integrate --rust-crate-dir ../flutter_bridge
    
    See also: https://github.com/fzyzcjy/flutter_rust_bridge/issues/1810#issuecomment-1987444840

--- gui_flutter/lib/main.dart
+++ gui_flutter/lib/main.dart
@@ -1,32 +1,61 @@
-import 'package:flutter/material.dart';
-import 'package:hash_calculator/my_app_state.dart';
-import 'package:hash_calculator/pages/my_home_page.dart';
-import 'package:provider/provider.dart';
+// The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
 
-void main() {
+// import 'package:flutter/material.dart';
+// import 'package:hash_calculator/my_app_state.dart';
+// import 'package:hash_calculator/pages/my_home_page.dart';
+// import 'package:provider/provider.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (context) => MyAppState(),
+//       child: MaterialApp(
+//         title: 'Hash Calculator',
+//         theme: ThemeData(
+//           brightness: Brightness.light,
+//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//           useMaterial3: true,
+//         ),
+//         darkTheme: ThemeData(
+//           brightness: Brightness.dark,
+//         ),
+//         themeMode: ThemeMode.dark,
+//         home: const MyHomePage(),
+//       ),
+//     );
+//   }
+// }
+//
+
+import 'package:flutter/material.dart';
+import 'package:hash_calculator/src/rust/api/simple.dart';
+import 'package:hash_calculator/src/rust/frb_generated.dart';
+
+Future<void> main() async {
+  await RustLib.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Hash Calculator',
-        theme: ThemeData(
-          brightness: Brightness.light,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
+        body: Center(
+          child: Text(
+              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
         ),
-        darkTheme: ThemeData(
-          brightness: Brightness.dark,
-        ),
-        themeMode: ThemeMode.dark,
-        home: const MyHomePage(),
       ),
     );
   }
